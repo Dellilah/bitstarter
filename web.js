1f774371644565192017738ebfd67e0a86d46479
Ubuntu <ubuntu@ip-172-31-22-53.us-west-2.compute.internal>
2013-07-10 10:48:10 +0000

    Added index

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var temp = text.toString('utf-8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(temp);
+  response.send('Ala');
 });
 
 var port = process.env.PORT || 5000;
