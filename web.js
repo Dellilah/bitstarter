545c9f084e3c3ac051d79ca3d1536e8b76aae167
Ubuntu <ubuntu@ip-172-31-22-53.us-west-2.compute.internal>
2013-07-10 10:52:51 +0000

    Added index

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var text = fs.readFileSync('index.html');
+vat tmp = text.toString();
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
